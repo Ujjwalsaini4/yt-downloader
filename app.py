caf641f6021c159714da6607872f40f7db0f91a2
Ujjwalsaini4 <ujjwalyt71@gmail.com>
2025-11-14 19:09:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -657,4 +657,4 @@ def cleanup(id):
     return jsonify({"status": "ok"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
